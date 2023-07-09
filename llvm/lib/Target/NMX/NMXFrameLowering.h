7fc16afb9d58374810be0bb049b0a1a86560078f
leikang123 <1013626230@qq.com>
2023-07-09 18:22:12 +0800

    update code

---
+++ llvm/lib/Target/NMX/NMXFrameLowering.h
@@ -0,0 +1,44 @@
+//===-- NMXFrameLowering.h - Frame Information for NMX --------*- C++ -*-===//
+//
+//                    The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_NMX_NMXFRAMELOWERING_H
+#define LLVM_LIB_TARGET_NMX_NMXFRAMELOWERING_H
+
+#include "NMX.h"
+#include "llvm/CodeGen/TargetFrameLowering.h"
+
+namespace llvm {
+class NMXSubtarget;
+
+class NMXFrameLowering : public TargetFrameLowering {
+protected:
+  const NMXSubtarget &STI;
+
+public:
+  explicit NMXFrameLowering(const NMXSubtarget &sti, unsigned Alignment)
+    : TargetFrameLowering(StackGrowsDown, Alignment, 0, Alignment),
+      STI(sti) { }
+
+  static const NMXFrameLowering *create(const NMXSubtarget &ST);
+
+  bool hasFP(const MachineFunction &MF) const override;
+
+  MachineBasicBlock::iterator
+  eliminateCallFramePseudoInstr(MachineFunction &MF,
+                                MachineBasicBlock &MBB,
+                                MachineBasicBlock::iterator I) const override;
+
+};
+
+// Create NMXFrameLowering objects.
+const NMXFrameLowering *createNMXSEFrameLowering(const NMXSubtarget &ST);
+
+} // End llvm namespace
+
+#endif
