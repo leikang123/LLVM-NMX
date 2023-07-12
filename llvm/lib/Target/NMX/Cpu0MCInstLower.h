febe00d72b0a58697392a8f23f11894ad3a91031
xuuhu <110091002+xuuhu@users.noreply.github.com>
2023-07-12 18:57:34 +0800

    Update code to  NMXMCInstLower.h

--- llvm/lib/Target/NMX/Cpu0MCInstLower.h
+++ llvm/lib/Target/NMX/Cpu0MCInstLower.h
@@ -1,4 +1,4 @@
-//===-- Cpu0MCInstLower.h - Lower MachineInstr to MCInst --------*- C++ -*-===//
+//===--NMXMCInstLower.h - Lower MachineInstr to MCInst --------*- C++ -*-===//
 //
 //                    The LLVM Compiler Infrastructure
 //
@@ -7,10 +7,10 @@
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIB_TARGET_CPU0_CPU0MCINSTLOWER_H
-#define LLVM_LIB_TARGET_CPU0_CPU0MCINSTLOWER_H
+#ifndef LLVM_LIB_TARGET_NMX_NMXMCINSTLOWER_H
+#define LLVM_LIB_TARGET_NMX_NMXMCINSTLOWER_H
 
-#include "MCTargetDesc/Cpu0MCExpr.h"
+#include "MCTargetDesc/NMXMCExpr.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/CodeGen/MachineOperand.h"
 #include "llvm/Support/Compiler.h"
@@ -21,14 +21,14 @@ class MCInst;
 class MCOperand;
 class MachineInstr;
 class MachineFunction;
-class Cpu0AsmPrinter;
+class NMXAsmPrinter;
 
-class LLVM_LIBRARY_VISIBILITY Cpu0MCInstLower {
+class LLVM_LIBRARY_VISIBILITY NMXMCInstLower {
   typedef MachineOperand::MachineOperandType MachineOperandType;
   MCContext *Ctx;
-  Cpu0AsmPrinter &AsmPrinter;
+  NMXAsmPrinter &AsmPrinter;
 public:
-  Cpu0MCInstLower(Cpu0AsmPrinter &asmprinter);
+  NMXMCInstLower(NMXAsmPrinter &asmprinter);
   void Initialize(MCContext *C);
   void Lower(const MachineInstr *MI, MCInst &OutMI) const;
   MCOperand LowerOperand(const MachineOperand &MO, unsigned offset = 0) const;
