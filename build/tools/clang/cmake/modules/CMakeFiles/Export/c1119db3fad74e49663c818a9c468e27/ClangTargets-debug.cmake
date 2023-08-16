3fa630c3a6291b6c24fe5a7600ab869c45055770
leikang123 <1013626230@qq.com>
2023-08-16 17:03:35 +0800

    add compiuler file

---
+++ build/tools/clang/cmake/modules/CMakeFiles/Export/c1119db3fad74e49663c818a9c468e27/ClangTargets-debug.cmake
@@ -0,0 +1,364 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "clangBasic" for configuration "Debug"
+set_property(TARGET clangBasic APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangBasic PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangBasic.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangBasic )
+list(APPEND _cmake_import_check_files_for_clangBasic "${_IMPORT_PREFIX}/lib/libclangBasic.a" )
+
+# Import target "clangLex" for configuration "Debug"
+set_property(TARGET clangLex APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangLex PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangLex.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangLex )
+list(APPEND _cmake_import_check_files_for_clangLex "${_IMPORT_PREFIX}/lib/libclangLex.a" )
+
+# Import target "clangParse" for configuration "Debug"
+set_property(TARGET clangParse APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangParse PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangParse.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangParse )
+list(APPEND _cmake_import_check_files_for_clangParse "${_IMPORT_PREFIX}/lib/libclangParse.a" )
+
+# Import target "clangAST" for configuration "Debug"
+set_property(TARGET clangAST APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangAST PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangAST.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangAST )
+list(APPEND _cmake_import_check_files_for_clangAST "${_IMPORT_PREFIX}/lib/libclangAST.a" )
+
+# Import target "clangDynamicASTMatchers" for configuration "Debug"
+set_property(TARGET clangDynamicASTMatchers APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangDynamicASTMatchers PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangDynamicASTMatchers.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangDynamicASTMatchers )
+list(APPEND _cmake_import_check_files_for_clangDynamicASTMatchers "${_IMPORT_PREFIX}/lib/libclangDynamicASTMatchers.a" )
+
+# Import target "clangASTMatchers" for configuration "Debug"
+set_property(TARGET clangASTMatchers APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangASTMatchers PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangASTMatchers.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangASTMatchers )
+list(APPEND _cmake_import_check_files_for_clangASTMatchers "${_IMPORT_PREFIX}/lib/libclangASTMatchers.a" )
+
+# Import target "clangCrossTU" for configuration "Debug"
+set_property(TARGET clangCrossTU APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangCrossTU PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangCrossTU.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangCrossTU )
+list(APPEND _cmake_import_check_files_for_clangCrossTU "${_IMPORT_PREFIX}/lib/libclangCrossTU.a" )
+
+# Import target "clangSema" for configuration "Debug"
+set_property(TARGET clangSema APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangSema PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangSema.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangSema )
+list(APPEND _cmake_import_check_files_for_clangSema "${_IMPORT_PREFIX}/lib/libclangSema.a" )
+
+# Import target "clangCodeGen" for configuration "Debug"
+set_property(TARGET clangCodeGen APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangCodeGen PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangCodeGen.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangCodeGen )
+list(APPEND _cmake_import_check_files_for_clangCodeGen "${_IMPORT_PREFIX}/lib/libclangCodeGen.a" )
+
+# Import target "clangAnalysis" for configuration "Debug"
+set_property(TARGET clangAnalysis APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangAnalysis PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangAnalysis.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangAnalysis )
+list(APPEND _cmake_import_check_files_for_clangAnalysis "${_IMPORT_PREFIX}/lib/libclangAnalysis.a" )
+
+# Import target "clangEdit" for configuration "Debug"
+set_property(TARGET clangEdit APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangEdit PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangEdit.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangEdit )
+list(APPEND _cmake_import_check_files_for_clangEdit "${_IMPORT_PREFIX}/lib/libclangEdit.a" )
+
+# Import target "clangRewrite" for configuration "Debug"
+set_property(TARGET clangRewrite APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangRewrite PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangRewrite.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangRewrite )
+list(APPEND _cmake_import_check_files_for_clangRewrite "${_IMPORT_PREFIX}/lib/libclangRewrite.a" )
+
+# Import target "clangARCMigrate" for configuration "Debug"
+set_property(TARGET clangARCMigrate APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangARCMigrate PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangARCMigrate.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangARCMigrate )
+list(APPEND _cmake_import_check_files_for_clangARCMigrate "${_IMPORT_PREFIX}/lib/libclangARCMigrate.a" )
+
+# Import target "clangDriver" for configuration "Debug"
+set_property(TARGET clangDriver APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangDriver PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangDriver.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangDriver )
+list(APPEND _cmake_import_check_files_for_clangDriver "${_IMPORT_PREFIX}/lib/libclangDriver.a" )
+
+# Import target "clangSerialization" for configuration "Debug"
+set_property(TARGET clangSerialization APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangSerialization PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangSerialization.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangSerialization )
+list(APPEND _cmake_import_check_files_for_clangSerialization "${_IMPORT_PREFIX}/lib/libclangSerialization.a" )
+
+# Import target "clangRewriteFrontend" for configuration "Debug"
+set_property(TARGET clangRewriteFrontend APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangRewriteFrontend PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangRewriteFrontend.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangRewriteFrontend )
+list(APPEND _cmake_import_check_files_for_clangRewriteFrontend "${_IMPORT_PREFIX}/lib/libclangRewriteFrontend.a" )
+
+# Import target "clangFrontend" for configuration "Debug"
+set_property(TARGET clangFrontend APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangFrontend PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangFrontend.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangFrontend )
+list(APPEND _cmake_import_check_files_for_clangFrontend "${_IMPORT_PREFIX}/lib/libclangFrontend.a" )
+
+# Import target "clangFrontendTool" for configuration "Debug"
+set_property(TARGET clangFrontendTool APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangFrontendTool PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangFrontendTool.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangFrontendTool )
+list(APPEND _cmake_import_check_files_for_clangFrontendTool "${_IMPORT_PREFIX}/lib/libclangFrontendTool.a" )
+
+# Import target "clangToolingCore" for configuration "Debug"
+set_property(TARGET clangToolingCore APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangToolingCore PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangToolingCore.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangToolingCore )
+list(APPEND _cmake_import_check_files_for_clangToolingCore "${_IMPORT_PREFIX}/lib/libclangToolingCore.a" )
+
+# Import target "clangToolingInclusions" for configuration "Debug"
+set_property(TARGET clangToolingInclusions APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangToolingInclusions PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangToolingInclusions.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangToolingInclusions )
+list(APPEND _cmake_import_check_files_for_clangToolingInclusions "${_IMPORT_PREFIX}/lib/libclangToolingInclusions.a" )
+
+# Import target "clangToolingRefactor" for configuration "Debug"
+set_property(TARGET clangToolingRefactor APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangToolingRefactor PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangToolingRefactor.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangToolingRefactor )
+list(APPEND _cmake_import_check_files_for_clangToolingRefactor "${_IMPORT_PREFIX}/lib/libclangToolingRefactor.a" )
+
+# Import target "clangToolingASTDiff" for configuration "Debug"
+set_property(TARGET clangToolingASTDiff APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangToolingASTDiff PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangToolingASTDiff.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangToolingASTDiff )
+list(APPEND _cmake_import_check_files_for_clangToolingASTDiff "${_IMPORT_PREFIX}/lib/libclangToolingASTDiff.a" )
+
+# Import target "clangTooling" for configuration "Debug"
+set_property(TARGET clangTooling APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangTooling PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangTooling.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangTooling )
+list(APPEND _cmake_import_check_files_for_clangTooling "${_IMPORT_PREFIX}/lib/libclangTooling.a" )
+
+# Import target "clangIndex" for configuration "Debug"
+set_property(TARGET clangIndex APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangIndex PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangIndex.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangIndex )
+list(APPEND _cmake_import_check_files_for_clangIndex "${_IMPORT_PREFIX}/lib/libclangIndex.a" )
+
+# Import target "clangStaticAnalyzerCore" for configuration "Debug"
+set_property(TARGET clangStaticAnalyzerCore APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangStaticAnalyzerCore PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangStaticAnalyzerCore.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangStaticAnalyzerCore )
+list(APPEND _cmake_import_check_files_for_clangStaticAnalyzerCore "${_IMPORT_PREFIX}/lib/libclangStaticAnalyzerCore.a" )
+
+# Import target "clangStaticAnalyzerCheckers" for configuration "Debug"
+set_property(TARGET clangStaticAnalyzerCheckers APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangStaticAnalyzerCheckers PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangStaticAnalyzerCheckers.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangStaticAnalyzerCheckers )
+list(APPEND _cmake_import_check_files_for_clangStaticAnalyzerCheckers "${_IMPORT_PREFIX}/lib/libclangStaticAnalyzerCheckers.a" )
+
+# Import target "clangStaticAnalyzerFrontend" for configuration "Debug"
+set_property(TARGET clangStaticAnalyzerFrontend APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangStaticAnalyzerFrontend PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangStaticAnalyzerFrontend.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangStaticAnalyzerFrontend )
+list(APPEND _cmake_import_check_files_for_clangStaticAnalyzerFrontend "${_IMPORT_PREFIX}/lib/libclangStaticAnalyzerFrontend.a" )
+
+# Import target "clangFormat" for configuration "Debug"
+set_property(TARGET clangFormat APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangFormat PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangFormat.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangFormat )
+list(APPEND _cmake_import_check_files_for_clangFormat "${_IMPORT_PREFIX}/lib/libclangFormat.a" )
+
+# Import target "clang" for configuration "Debug"
+set_property(TARGET clang APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clang PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/clang-8"
+  )
+
+list(APPEND _cmake_import_check_targets clang )
+list(APPEND _cmake_import_check_files_for_clang "${_IMPORT_PREFIX}/bin/clang-8" )
+
+# Import target "clang-format" for configuration "Debug"
+set_property(TARGET clang-format APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clang-format PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/clang-format"
+  )
+
+list(APPEND _cmake_import_check_targets clang-format )
+list(APPEND _cmake_import_check_files_for_clang-format "${_IMPORT_PREFIX}/bin/clang-format" )
+
+# Import target "clangHandleCXX" for configuration "Debug"
+set_property(TARGET clangHandleCXX APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangHandleCXX PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangHandleCXX.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangHandleCXX )
+list(APPEND _cmake_import_check_files_for_clangHandleCXX "${_IMPORT_PREFIX}/lib/libclangHandleCXX.a" )
+
+# Import target "clangHandleLLVM" for configuration "Debug"
+set_property(TARGET clangHandleLLVM APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clangHandleLLVM PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclangHandleLLVM.a"
+  )
+
+list(APPEND _cmake_import_check_targets clangHandleLLVM )
+list(APPEND _cmake_import_check_files_for_clangHandleLLVM "${_IMPORT_PREFIX}/lib/libclangHandleLLVM.a" )
+
+# Import target "clang-import-test" for configuration "Debug"
+set_property(TARGET clang-import-test APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clang-import-test PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/clang-import-test"
+  )
+
+list(APPEND _cmake_import_check_targets clang-import-test )
+list(APPEND _cmake_import_check_files_for_clang-import-test "${_IMPORT_PREFIX}/bin/clang-import-test" )
+
+# Import target "clang-rename" for configuration "Debug"
+set_property(TARGET clang-rename APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clang-rename PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/clang-rename"
+  )
+
+list(APPEND _cmake_import_check_targets clang-rename )
+list(APPEND _cmake_import_check_files_for_clang-rename "${_IMPORT_PREFIX}/bin/clang-rename" )
+
+# Import target "clang-refactor" for configuration "Debug"
+set_property(TARGET clang-refactor APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(clang-refactor PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/clang-refactor"
+  )
+
+list(APPEND _cmake_import_check_targets clang-refactor )
+list(APPEND _cmake_import_check_files_for_clang-refactor "${_IMPORT_PREFIX}/bin/clang-refactor" )
+
+# Import target "libclang" for configuration "Debug"
+set_property(TARGET libclang APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libclang PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/lib/libclang.dylib"
+  IMPORTED_SONAME_DEBUG "@rpath/libclang.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets libclang )
+list(APPEND _cmake_import_check_files_for_libclang "${_IMPORT_PREFIX}/lib/libclang.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
