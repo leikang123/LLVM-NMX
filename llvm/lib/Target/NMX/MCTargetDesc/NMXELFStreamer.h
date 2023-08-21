3a72c213731166ea276fc840af2753a67ca2b4a7
leikang123 <1013626230@qq.com>
2023-08-21 18:58:23 +0800

    add commant

--- llvm/lib/Target/NMX/MCTargetDesc/NMXELFStreamer.h
+++ llvm/lib/Target/NMX/MCTargetDesc/NMXELFStreamer.h
@@ -20,6 +20,14 @@
 
 #include <memory>
 
+
+/**]
+ * 定义了一个叫 Cpu0ELFStreamer 的类，继承自 MCELFStreamer 类。另外定义了这个类的工厂函数 `
+ * createCpu0ELFStreamer()`，用来返回其对象。ELFStreamer 对象会注册到后端模块中。
+
+TargetStreamer 和 ELFStreamer 在生成 ELF 文件中同时起作用，ELFStreamer 是我们自定义的一个类，
+在其中可以做一些钩子来调整输出内容。
+*/
 namespace llvm {
 
 class MCAsmBackend;
